d78170a641bff7993a3a86e5c8102e16d9cdbe6e
omer.borhan <omer.borhan@turkai.com>
2020-07-09 13:59:07 +0300

    custom train added with supported custom dataset

--- efficientdet/dataset.py
+++ efficientdet/dataset.py
@@ -4,8 +4,111 @@ import numpy as np
 
 from torch.utils.data import Dataset, DataLoader
 from pycocotools.coco import COCO
-import cv2
+from cv2 import cv2
+from typing import List
+from tqdm import tqdm
+import random
 
+class CustomDataset(Dataset):
+    """Parses dataset with format of:
+
+        .../<root_dir>/
+            annotations/
+                <file_id_1>.txt
+                <file_id_2>.txt
+                <file_id_3>.txt
+            data/
+                <file_id_1>.jpg
+                <file_id_2>.jpg
+                <file_id_3>.jpg
+
+        annotation .txt file format:
+            <label_name> xmin,ymin,xmax,ymax
+            <label_name> xmin,ymin,xmax,ymax
+            <label_name> xmin,ymin,xmax,ymax
+            ...
+    """
+    def __init__(self, root_dir:str, categories:List, transform=None, shuffle:bool=False):
+        super(CustomDataset,self).__init__()
+        self.root_dir = root_dir
+        self.transform = transform
+        self.categories = categories
+        self.ids,self.targets = self._parse_data(self.root_dir, self.categories, shuffle=shuffle)
+
+    @staticmethod
+    def _parse_data(root_dir:str, categories:List,
+            data_prefix:str='data', ann_prefix:str='annotations', shuffle:bool=False):
+        data_path = os.path.join(root_dir, data_prefix)
+        ann_path = os.path.join(root_dir, ann_prefix)
+
+        data_files = sorted(os.listdir(data_path))
+        ann_files = sorted(os.listdir(ann_path))
+
+        def get_idx(raw_idx:str): return os.path.splitext(os.path.basename(raw_idx))[0]
+
+        def parse_ann(a_p_f:str, ct:List):
+            t = []
+
+            with open(a_p_f, "r") as foo:
+                rows = foo.read().split("\n")
+            try:
+                for row in rows:
+                    if row == '': continue
+                    *label,box = row.split(" ")
+                    label = " ".join(label)
+                    box = [float(b) for b in box.split(",")]
+                    assert len(box) == 4,f"box length is not correct at annotation file {a_p_f}"
+                    try:
+                        cls_idx = ct.index(label)
+                    except ValueError:
+                        #print(f"warning, {label} not in the category list at annotation file {a_p_f}")
+                        continue
+                    x = max(0,box[0])
+                    y = max(0,box[1])
+                    w = box[2] - x
+                    h = box[3] - y
+                    # skip boxes that width or height is lower than 1
+                    if box[2] < 1 or box[3] < 1: continue
+
+                    t.append([x,y,w,h,cls_idx])
+
+            except Exception as e:
+                print(f"corrupted annotation file: {a_p_f}\n",e)
+                return None
+
+            if len(t) > 0: return np.array(t, dtype=np.float32)
+
+        ids = []
+        for data_file,ann_file in tqdm(zip(data_files,ann_files), desc='checking dataset...', total=len(data_files)):
+            assert get_idx(data_file) == get_idx(ann_file),f"data and annotation do not match {data_file} != {ann_file}"
+            ann = parse_ann(os.path.join(ann_path,ann_file), categories)
+
+            if isinstance(ann,type(None)): continue
+
+            ids.append( (os.path.join(data_path,data_file), ann) )
+
+        if shuffle: random.shuffle(ids)
+
+        ids,targets = zip(*ids)
+        return ids,targets
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx:int):
+        sample = { 'img': self._load_img(self.ids[idx]), 'annot': self.targets[idx]}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def _load_img(self, img_path:str):
+        path = os.path.join(self.root_dir, img_path)
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        return img.astype(np.float32) / 255.
 
 class CocoDataset(Dataset):
     def __init__(self, root_dir, set='train2017', transform=None):
@@ -42,8 +145,10 @@ class CocoDataset(Dataset):
         img = self.load_image(idx)
         annot = self.load_annotations(idx)
         sample = {'img': img, 'annot': annot}
+
         if self.transform:
             sample = self.transform(sample)
+
         return sample
 
     def load_image(self, image_index):
@@ -109,7 +214,7 @@ def collater(data):
 
 class Resizer(object):
     """Convert ndarrays in sample to Tensors."""
-    
+
     def __init__(self, img_size=512):
         self.img_size = img_size
 
